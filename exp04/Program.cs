4ab71eb6e5157a6d9d86f33bd3dd96006c6aa3f7
Denis <Siyanov.d@sar.garm.ru>
2022-01-09 21:55:55 +0400

    вывод максимального числа

---
+++ exp04/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 51111;
+int b = 711;
+int c = 8111;
+int max =0;
+
+if (a > max)
+ {
+     max = a;
+}
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+ {
+    max = c;
+}
+Console.WriteLine(max);
