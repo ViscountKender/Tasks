cb425c90bffeba9a4bb96cb77be5877c2ad53dbf
Denis <Siyanov.d@sar.garm.ru>
2022-01-09 22:48:35 +0400

    вывод наибольшей цифры числа

---
+++ exp11/Program.cs
@@ -0,0 +1,20 @@
+﻿Random random = new Random();
+int n ;
+n = random.Next(10,99);
+Console.WriteLine("Рандомное число " + n);
+int number = n % 10;
+int number1 = n / 10;
+if ( number1 > number)
+{
+    Console.WriteLine("Наибольшая цифра " + number1);
+    
+}
+if (number > number1)
+{
+    Console.WriteLine("Наибольшая цифра " + number);
+}
+if (number == number1)
+{
+     Console.WriteLine("числа одинаковые");
+}
+
