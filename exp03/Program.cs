150a11dc01791f6b9d862a251812c9a426928ae3
Denis <Siyanov.d@sar.garm.ru>
2022-01-09 21:53:40 +0400

    номер недели

---
+++ exp03/Program.cs
@@ -0,0 +1,4 @@
+﻿String [] week = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+Console.WriteLine("Введите номер дня недели - ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(week[number -1]);
