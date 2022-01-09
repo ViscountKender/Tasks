45876722cccfee37c4449a7df97c9c551b519433
Denis <Siyanov.d@sar.garm.ru>
2022-01-09 22:00:09 +0400

    проверка на четность

---
+++ exp06/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ведите число ");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x % 2 == 0)
+{
+    Console.WriteLine("Число четное  ");
+    
+}
+else
+{
+Console.WriteLine("Число нечетное  ");
+
+}
