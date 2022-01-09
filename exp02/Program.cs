1d63e5559ba58d4830216b568a02f334f236d2a0
Denis <Siyanov.d@sar.garm.ru>
2022-01-09 21:49:49 +0400

    большее или меньшее число

---
+++ exp02/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2)
+{
+    Console.WriteLine("Числа равны ");
+}
+if (number1 > number2)
+{
+    Console.WriteLine($"Число {number1} больше числа {number2}");
+}
+
+if (number1 < number2)
+{
+   Console.WriteLine($"Число {number1} меньше числа {number2}");
+}
