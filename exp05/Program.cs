12f1c52635e3eb76a04dd55649a5de0251a0af5f
Denis <Siyanov.d@sar.garm.ru>
2022-01-09 21:58:43 +0400

    функция

---
+++ exp05/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите х не равный нулю ");
+int x = int.Parse(Console.ReadLine());
+if (x == 0)
+{
+    Console.WriteLine("Вай ты баран ввел  х равный нулю ");
+
+}
+int y = 3 / x * x * x - 8;
+Console.WriteLine("F = " + y);
