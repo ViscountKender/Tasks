48d13174626d94c5af85d5c8d3427f913995f1f5
Denis <Siyanov.d@sar.garm.ru>
2022-01-11 01:36:10 +0400

    вывод третьей цифры числа

---
+++ exp14/Program.cs
@@ -0,0 +1,10 @@
+﻿Random random = new Random();
+int n ;
+n = random.Next(1,99999);
+if (n <100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+ string Stringnumer = Convert.ToString(n);
+Console.WriteLine(n);
+Console.WriteLine(" Третья цифра числа " + Stringnumer[2]);
