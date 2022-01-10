82f105300c4794a3f8675a3a615d2c509ceb6656
Denis <Siyanov.d@sar.garm.ru>
2022-01-10 22:23:37 +0400

    квадрат числа

--- exp0/Program.cs
+++ exp0/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+number = number * number;
+Console.WriteLine(number);
