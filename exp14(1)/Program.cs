46c1cd33db20ebc29e3d7ddf6e2f291beb780711
Denis <Siyanov.d@sar.garm.ru>
2022-01-11 02:20:21 +0400

    вывод третьей цифры, второй вариант

---
+++ exp14(1)/Program.cs
@@ -0,0 +1,20 @@
+﻿Random random = new Random();
+int n ;
+n = random.Next(1,99999);
+int number = n;
+if (n <100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else 
+{
+    for (int i = 0; n  > 1000; i++)
+{
+    n = n / 10;
+}
+n = n % 10;
+Console.WriteLine(number);
+Console.WriteLine("Третья цифра числа " + n);
+
+}
+
