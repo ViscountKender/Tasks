f202364b8c836e9f800732269d1b5f866227218d
Denis <Siyanov.d@sar.garm.ru>
2022-01-11 00:04:42 +0400

    кратность числа

---
+++ exp13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("введите первое число ");
+decimal num1 = Convert.ToInt64(Console.ReadLine());
+Console.WriteLine("введите второе число ");
+decimal num2 = Convert.ToInt64(Console.ReadLine());
+decimal result = (num1 % num2);
+if ( result == 0 )
+{
+    Console.WriteLine("первое число кратно второму ");
+}
+else 
+{
+    Console.WriteLine("число не кратно остаток от деления " + result);
+}
+    
