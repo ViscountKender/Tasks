5315c986f57497f4f3efe879ba23ce64d14cec0f
Denis <Siyanov.d@sar.garm.ru>
2022-01-11 19:48:21 +0400

    проверка на кратность 7 и 23

---
+++ exp15/Program.cs
@@ -0,0 +1,15 @@
+﻿Random random = new Random();
+int n ;
+n = random.Next(1,99999);
+int number = n;
+int result1 = n % 7;
+int result2 = n % 23;
+if (result1 == 0 && result2 == 0 )
+{
+    Console.WriteLine($"число {n} кратно 7 и 23 ");
+}
+
+else
+{
+    Console.WriteLine($"число {n} некратно 7 и 23");
+}
