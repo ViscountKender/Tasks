c1523fd20dcb75963f1774d4d68762766664bcf0
Denis <Siyanov.d@sar.garm.ru>
2022-01-11 22:19:10 +0400

    проверка дня недели

---
+++ exp16/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите номер дня недели - ");
+long number = Convert.ToInt32(Console.ReadLine());
+if (0 >= number || number > 8  )
+{
+    Console.WriteLine(" Неверный номер недели");
+    Environment.Exit(0);
+}
+if (number == 6 || number == 7 )
+{
+    Console.WriteLine(" Выходной день");
+}
+else
+{
+    Console.WriteLine(" Рабочий день");
+}
